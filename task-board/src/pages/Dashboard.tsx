51066d02c6b1cb578519fbd79e3bbe67409e9470
jairoVL <jairovillalba084@gmail.com>
2025-07-09 22:37:00 -0300

    creacion de dashboard sin funcionalidad

--- task-board/src/pages/Dashboard.tsx
+++ task-board/src/pages/Dashboard.tsx
@@ -1,10 +1,27 @@
-const Dashboard = () => {
+import React from "react";
+import styles from "../styles/Dashboard.module.scss";
+
+const Dashboard: React.FC = () => {
   return (
-    <div style={{ padding: 20 }}>
-      <h2>Dashboard de Tareas</h2>
-      <p></p>
+    <div className={styles.dashboard}>
+      <div className={styles.column}>
+        <h3>Por hacer</h3>
+        <div className={styles.task}>Estudiar React</div>
+        <div className={styles.task}>Diseñar wireframe</div>
+      </div>
+
+      <div className={styles.column}>
+        <h3>En progreso</h3>
+        <div className={styles.task}>Maquetar dashboard</div>
+      </div>
+
+      <div className={styles.column}>
+        <h3>Completado</h3>
+        <div className={styles.task}>Crear repositorio</div>
+      </div>
     </div>
   );
 };
 
 export default Dashboard;
+
